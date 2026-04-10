bb071ffc9772363251d6262fdc7cd819f35bd281
abha-hue <guptabhay577@gmail.com>
2026-04-10 21:03:04 +0530

    Add .gitignore and initial web scraping script

---
+++ gets.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+import queue
+URL = "https://www.geeksforgeeks.org/"
+resp = requests.get(URL)
+soup = BeautifulSoup(resp.content, 'html.parser')
+ls = soup.find_all('a')
+
+for i in ls:
+    queue.put(i.get('href'))
+    print(i.get('href'))
+
+
+# print(soup.prettify())
